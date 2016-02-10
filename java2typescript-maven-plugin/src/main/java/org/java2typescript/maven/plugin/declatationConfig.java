9ed9305c56d353e7eda4c84fd24f2981ae2c9000
Lukasz Janczara <Lukasz.Janczara@cgm.com>
2016-02-10 11:10:41 +0100

    add reference path configuration

---
+++ java2typescript-maven-plugin/src/main/java/org/java2typescript/maven/plugin/declatationConfig.java
@@ -0,0 +1,65 @@
+/*
+ * declatationConfig
+ * Date of creation: 2016-02-10
+ * 
+ * Copyright (c) CompuGROUP Software GmbH,
+ * This software is the confidential and proprietary information of
+ * CompuGROUP Software GmbH. You shall not disclose such confidential
+ * information and shall use it only in accordance with the terms of
+ * the license agreement you entered into with CompuGROUP Software GmbH.
+ */
+package org.java2typescript.maven.plugin;
+
+/**
+ * @author £ukasz Janczara, PL
+ */
+public class declatationConfig {
+
+    private String complexTypePackage;
+
+    private String moduleName;
+
+    private String referenceDeclaration;
+
+    private boolean generateExternalClasses;
+
+    public declatationConfig() {
+    }
+
+    public declatationConfig(String complexTypePackage, String moduleName) {
+        this.complexTypePackage = complexTypePackage;
+        this.moduleName = moduleName;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getComplexTypePackage() {
+        return complexTypePackage;
+    }
+
+    public void setComplexTypePackage(String complexTypePackage) {
+        this.complexTypePackage = complexTypePackage;
+    }
+
+    public String getReferenceDeclaration() {
+        return referenceDeclaration;
+    }
+
+    public void setReferenceDeclaration(String referenceDeclaration) {
+        this.referenceDeclaration = referenceDeclaration;
+    }
+
+    public boolean isGenerateExternalClasses() {
+        return generateExternalClasses;
+    }
+
+    public void setGenerateExternalClasses(boolean generateExternalClasses) {
+        this.generateExternalClasses = generateExternalClasses;
+    }
+}
